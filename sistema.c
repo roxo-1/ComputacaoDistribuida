0e2ab70b398fa23603b817a9e44469d9dabde296
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2026-09-02 04:12:36 +0000

    COMMIT tentei fazer o adicionar mas nâo consegui ... são 1h da manhã e o código parou de fazer sentido pra mim

--- sistema.c
+++ sistema.c
@@ -14,35 +14,93 @@ typedef struct {
     float preco;
     int quantidade;
 } Produto;
- //Funções
-void adicionar_produto(){
+
+//Funções
+Produto* criacao_vetor(){
+    Produto *vetor;
+    int n = 250;
+    vetor = (Produto*) malloc(n * sizeof(Produto));
+    
+    if (vetor == NULL) {
+        printf("Erro: Falha na alocação de memória!\n");
+        return NULL;
+    }
+    return vetor;
+}
+
+void adicionar_produto(Produto *v,int tamanho){
+    //variáveis pro código funcionar
+    char nome[100];
+    float preco;
+    int qtd, id;
+    int novo_tamanho = tamanho+1;
     /*
     - O usuário informa: nome, preço e quantidade.
     - O sistema gera automaticamente um código único (incremental).
     - O vetor de produtos deve ser **realocado dinamicamente** para acomodar o novo produto (use `realloc`).
-    - O nome do produto deve ter memória alocada individualmente.
-    */
-   printf("--- Adicionar Produto ---");
+    - O nome do produto deve ter memória alocada individualmente. (não entendi isso)*/
+
+    //Gerar código único
+    // Para calcular o tamanho real do vetor+1
+    int cont;
+    for (int i = 0; i<sizeof(v); i++){
+        cont++;
+    }
+    id = cont+1;
+
+    //Pegas as infos do produto segundo o usuário
+    printf("--- Adicionar Produto ---");
+    printf("\nNome: ");
+    scanf("%s", nome);
+    printf("\nPreço: ");
+    scanf("%f", &preco);
+    printf("\nQuantidade: ");
+    scanf("%d", &qtd);
+
+    //Cria o produto  na struct
+    Produto p;
+    p.codigo=id;
+    p.nome=nome;
+    p.preco=preco;
+    p.quantidade=qtd;
+
+
+    //realoca vetor
+    v = (Produto*) realloc(v, novo_tamanho * sizeof(Produto));
+    
+    if (v == NULL) {
+        printf("Erro: Falha na realocação!\n");
+        return;
+    }
+
+    //Adiciona o produto no vetor
+    for (int i = 0; i < novo_tamanho; i++) {
+        if (i == novo_tamanho) {
+            v[i] = p;
+        }
+    }
+
+
 }
-void listar_produtos(){
+void listar_produtos(Produto *v){
     /*- Exibe todos os produtos cadastrados em formato tabular.
     - Mostra: código, nome, preço, quantidade e **valor em estoque** (preço × quantidade).
     - Ao final, exibe o **valor total do estoque** (soma de todos os valores em estoque).*/
     printf("--- Lista de Produtos ---");
 }
-Produto* buscar_produto(){
+Produto* buscar_produto(Produto *v){
     /*- O usuário informa um código.
     - O sistema busca e exibe as informações do produto, ou informa que não foi encontrado.
     - A busca deve ser implementada em uma **função separada** que retorne um ponteiro para o produto encontrado (ou `NULL`).*/
     return 0;
 }
-void atualizar_estoque(){
+void atualizar_estoque(Produto *v){
     /*- O usuário informa o código do produto e a nova quantidade.
     - O sistema deve usar a função de busca (item 3) para localizar o produto.
     - Utilize **passagem por referência** para modificar a quantidade.*/
     printf("--- Atualizar Estoque ---");
 }
-void remover_produto(){
+void remover_produto(Produto *v){
     /*- O usuário informa o código do produto a ser removido.
     - O sistema deve:
     1. Localizar o produto no vetor
@@ -52,7 +110,8 @@ void remover_produto(){
     - **Atenção especial** à ordem de liberação de memória!*/
     printf("--- Atualizar Estoque ---");
 }
-void liberar_memoria(){
+void liberar_memoria(Produto *v){
+    // ese liberar memória é de um produto em especifico
     /*- Antes de encerrar, o programa **deve liberar toda a memória alocada**:
     - Primeiro: liberar cada `nome` individualmente
     - Depois: liberar o vetor de produtos
@@ -62,8 +121,10 @@ void liberar_memoria(){
 
 
 int main(){
+    Produto *vetor_produtos = criacao_vetor();
     int opcao = 0;
-     //Mostra e lê as opções do menu
+    int tamanho = 1;
+    //Mostra e lê as opções do menu
     printf("========================================\n");
     printf("    SISTEMA DE CADASTRO DE PRODUTOS     \n");
     printf("========================================\n");
@@ -79,24 +140,24 @@ int main(){
         }
         //redirecionando para as funções certas, menos a opção de sair que ficou para fora
         if (opcao == 1){
-            adicionar_produto();
+            adicionar_produto(vetor_produtos, tamanho);
         }
         else if (opcao == 2 ){
-            listar_produtos();
+            listar_produtos(vetor_produtos);
         }
         else if (opcao == 3){
-            buscar_produto();
+            buscar_produto(vetor_produtos);
         }
         else if (opcao == 4){
-            atualizar_estoque();
+            atualizar_estoque(vetor_produtos);
         }
         else if (opcao == 5){
-            remover_produto();
+            remover_produto(vetor_produtos);
         }
     }
     //opção 6 menu, Sair
     if( opcao == 6){
-        liberar_memoria();
+        liberar_memoria(vetor_produtos);
     }
     return 0;
 }
